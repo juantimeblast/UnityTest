5570eadb97c3a5486c6e9433f2d103114809e9ab
JuanRodriguezGiles <juanrodriguezgiles@pathfindersstudio.com>
2024-04-06 14:15:25 -0300

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region EXPOSED_FIELDS
+
+    #endregion
+
+    #region PRIVATE_FIELDS
+
+    #endregion
+
+    #region PUBLIC_METHODS
+
+    #endregion
+
+    #region PRIVATE_METHODS
+
+    #endregion
+}
